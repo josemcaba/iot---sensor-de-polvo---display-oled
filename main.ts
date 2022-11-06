65a904a197a785ff154a159452b296724dc00a0f
josemcaba <96978309+josemcaba@users.noreply.github.com>
2022-11-06 21:52:36 +0100

    Muestreo cada minuto

--- main.ts
+++ main.ts
@@ -3,5 +3,5 @@ basic.forever(function () {
     OLED.clear()
     OLED.writeString("Polvo (ug/m3): ")
     OLED.writeNum(Environment.ReadDust(DigitalPin.P6, AnalogPin.P1))
-    basic.pause(2000)
+    basic.pause(60000)
 })
